254bac9d181b50f55c1c7303de09f8432c329be1
KimSeJin <sejin1489@gmail.com>
2022-10-06 17:37:49 +0900

    테이블 리스트불러오기 기능 추가

---
+++ MDBToExcel-master/MDBtoXL/MdbToXl/MdbToXlDlg.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include <vector>
+#include <algorithm>
+// MdbToXlDlg.h: 헤더 파일
+//#include "PictureEx.h"
+#include "XLAutomation.h"
+#include "XLEzAutomation.h"
+#include <typeinfo>
+#include <afxdb.h>
+#include "CPictureEX.h"
+
+#pragma once
+using namespace std;
+class CMdbToXlDlgAutoProxy;
+
+typedef struct FieldINFO
+{
+	CString strFieldName ;	//원본 이름
+	CString strExcelName;	//엑셀 이름(변경됨)
+	int nFieldIdcnt;		//필드 인덱스
+	int nExcelIdcnt;		//엑셀 인덱스
+} FieldINFO;
+
+// CMdbToXlDlg 대화 상자
+class CMdbToXlDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CMdbToXlDlg);
+	friend class CMdbToXlDlgAutoProxy;
+
+// 생성입니다.
+public:
+	CMdbToXlDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	virtual ~CMdbToXlDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MDBTOXL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	CMdbToXlDlgAutoProxy* m_pAutoProxy;
+	HICON m_hIcon;
+
+	BOOL CanExit();
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+
+	DECLARE_MESSAGE_MAP()
+public:
+	
+	afx_msg void OnLvnItemchangedFieldlist(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedbtnfileload();
+	afx_msg void OnBnClickedbtninput();
+	afx_msg void OnCbnSelchangeTable();
+	afx_msg void OnBnClickedbtnsave();
+	afx_msg void OnBnClickedbtnupdate();
+	afx_msg void OnNMClickExcellist(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedbtnallselect();
+	afx_msg void OnBnClickedbtncancel();
+	afx_msg void OnNMClickFieldlist(NMHDR* pNMHDR, LRESULT* pResult);
+
+	//-----------------------------------
+	// MDB 관련
+	CDatabase m_DB;
+	CRecordset* m_pRecordset;
+
+	BOOL m_bConn;
+		
+	vector<FieldINFO> m_VstTrueFieldValue;	//실제 데이터 원본
+	vector<FieldINFO> m_VstExcelValue;		//Excellist 데이터 //실제 excel에 나올 데이터
+	FieldINFO m_stFieldInfo;				//이름과 번호 구조체
+
+	CString m_strPathName;
+	CString m_strFileName;
+		
+	CString m_strTable;
+	CString m_strPW;
+
+	void CallDBTable();
+
+	//-----------------------------------
+	// 엑셀 관련
+	CFileDialog* m_pExcelDlg ;		// excel 다이얼로그 포인터 변수
+	//-----------------------------------
+	// GUI 관련
+	int m_nXlRowNum;				// excel 행 증가	
+	int m_nidx;						// 마우스 포커스 유지 변수
+
+	CString strClip(CString str);
+	CString m_strEdit;
+	CComboBox m_ctrlTable;
+	CListCtrl m_ctrlFieldList;
+	CListCtrl m_ctrlExcelList;
+	CProgressCtrl m_ctrlProgress;
+
+	//--------------------------------------
+	// 로고 관련
+	CPictureEx m_ctrlROGO;
+};
